14c4987a027306b3da58d95f31b1752786d30be7
oluwatoyin oluwaferanmi john <oluwatoyinferanmijohn>
2022-06-21 19:36:47 +0100

    changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function openNav() {
-    document.getElementById("mySidebar").style.width = "200px";
-    document.getElementById("main").style.marginLeft = "200px";
+    document.getElementById("mySidebar").style.width = "190px";
+    document.getElementById("main").style.marginLeft = "250px";
 }
 
 function closeNav() {
